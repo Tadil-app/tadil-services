e794a4126ef1eb8566701a35a3ed11716ec0c8cd
ChikhaouiFaycel <faycel@octoplus.tech>
2025-11-06 13:53:57 +0100

    remove module routing

--- apps/tadil-api/src/app/app.module.ts
+++ apps/tadil-api/src/app/app.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
 import { SectionsModule } from './sections/sections.module';
 
 @Module({
-  imports: [
-    SectionsModule,
-    RouterModule.register([
-      {
-        path: 'sections',
-        module: SectionsModule,
-      },
-    ]),
-  ],
+  imports: [SectionsModule],
   controllers: [],
   providers: [],
 })
