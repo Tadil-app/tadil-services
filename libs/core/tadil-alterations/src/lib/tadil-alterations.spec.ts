61775238ff1e6c18d9d4d2acf3868dd8ed5fe6df
ChikhaouiFaycel <faycel@octoplus.tech>
2025-11-06 14:23:01 +0100

    add alterations domain module

---
+++ libs/core/tadil-alterations/src/lib/tadil-alterations.spec.ts
@@ -0,0 +1,7 @@
+import { tadilAlterations } from './tadil-alterations.js';
+
+describe('tadilAlterations', () => {
+  it('should work', () => {
+    expect(tadilAlterations()).toEqual('tadil-alterations');
+  });
+});
---
+++ libs/core/tadil-alterations/src/lib/tadil-alterations.ts
@@ -0,0 +1,3 @@
+export function tadilAlterations(): string {
+  return 'tadil-alterations';
+}
