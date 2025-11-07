ee03ba165822e793a0dd15f844565a68e37205f1
ChikhaouiFaycel <faycel@octoplus.tech>
2025-11-07 22:44:29 +0100

    remove class validator from display dtos

--- apps/tadil-api/src/app/models/dtos/displayModel.dto.ts
+++ apps/tadil-api/src/app/models/dtos/displayModel.dto.ts
@@ -1,30 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
 import { DisplaySectionDTO } from '.';
 
 export class DisplayModelDTO {
   @ApiProperty()
-  @IsNotEmpty()
   id!: string;
   @ApiProperty()
-  @IsNotEmpty()
   imageFileId!: string;
   @ApiProperty()
-  @IsNotEmpty()
   englishName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   arabicName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   hindiName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   urduName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   bengaliName!: string;
   @ApiProperty({ type: () => DisplaySectionDTO, isArray: true })
-  @IsNotEmpty()
   sections!: DisplaySectionDTO[];
 }
