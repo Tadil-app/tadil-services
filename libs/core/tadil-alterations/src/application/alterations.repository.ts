33ed4bfeee662727858361b641340377c91f02c9
ChikhaouiFaycel <faycel@octoplus.tech>
2025-11-08 00:05:56 +0100

    add undefined to handle no data found

--- libs/core/tadil-alterations/src/application/alterations.repository.ts
+++ libs/core/tadil-alterations/src/application/alterations.repository.ts
@@ -1,7 +1,7 @@
 import { Alteration } from './alteration.model';
 
 export interface AlterationsRepository {
-  getAlterationById(id: string): Promise<Alteration>;
+  getAlterationById(id: string): Promise<Alteration | undefined>;
   createAlteration(alteration: Alteration): Promise<void>;
   updateAlteration(alteration: Alteration): Promise<void>;
   deleteAlteration(id: string): Promise<void>;
