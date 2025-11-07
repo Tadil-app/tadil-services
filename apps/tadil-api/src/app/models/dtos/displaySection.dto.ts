0b339c776f03fcfff93b3f096c2b9958ef5aaef0
ChikhaouiFaycel <faycel@octoplus.tech>
2025-11-07 22:45:22 +0100

    - remove class validator from display dto - move Point class to index.ts to be shared across dependant dtos

--- apps/tadil-api/src/app/models/dtos/displaySection.dto.ts
+++ apps/tadil-api/src/app/models/dtos/displaySection.dto.ts
@@ -1,33 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
-
-export class Point {
-  @ApiProperty()
-  x!: number;
-  @ApiProperty()
-  y!: number;
-}
+import { Point } from '.';
 
 export class DisplaySectionDTO {
   @ApiProperty()
-  @IsNotEmpty()
   id!: string;
   @ApiProperty()
-  @IsNotEmpty()
   englishName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   arabicName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   hindiName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   urduName!: string;
   @ApiProperty()
-  @IsNotEmpty()
   bengaliName!: string;
   @ApiProperty({ type: () => Point, isArray: true })
-  @IsNotEmpty()
   coordinates!: Point[];
 }
--- apps/tadil-api/src/app/models/dtos/index.ts
+++ apps/tadil-api/src/app/models/dtos/index.ts
@@ -1,3 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export * from './displayModel.dto';
 export * from './createModel.dto';
 export * from './displaySection.dto';
+export * from './createSection.dto';
+
+export class Point {
+  @ApiProperty()
+  x!: number;
+  @ApiProperty()
+  y!: number;
+}
