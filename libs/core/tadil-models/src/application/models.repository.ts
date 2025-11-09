aec38b76050dbcb77856811360de813b31e4c8f5
ChikhaouiFaycel <faycel@octoplus.tech>
2025-11-09 10:55:24 +0100

    omit image file id and sections from updateModel

--- libs/core/tadil-models/src/application/models.repository.ts
+++ libs/core/tadil-models/src/application/models.repository.ts
@@ -3,7 +3,7 @@ import { Model, Section } from './model.model';
 export interface ModelsRepository {
   getModelById(id: string): Promise<Model | undefined>;
   createModel(model: Model): Promise<void>;
-  updateModel(model: Model): Promise<void>;
+  updateModel(model: Omit<Model, 'imageFileId' | 'sections'>): Promise<void>;
   deleteModel(id: string): Promise<void>;
   getSectionById(id: string): Promise<Section | undefined>;
   addSection(section: Section): Promise<void>;
