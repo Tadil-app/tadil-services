58eac311e49086762e1ceb4cd4454ff93b2311fa
ChikhaouiFaycel <faycel@octoplus.tech>
2025-11-10 13:02:38 +0100

    implement services module repository

---
+++ libs/infra/tadil-database/src/repositories/services/services.repository.ts
@@ -0,0 +1,72 @@
+import { Service, ServicesRepository } from '@tadil-services';
+import { DbClient } from '../../dbClient';
+export class PrismaServicesRepository implements ServicesRepository {
+  constructor(private readonly _db: DbClient) {}
+
+  async getServiceById(id: string): Promise<Service | undefined> {
+    const service = await this._db.service.findUnique({
+      where: { id },
+      include: {
+        sections: { select: { id: true } },
+        alterations: { select: { id: true } },
+      },
+    });
+
+    if (!service) return undefined;
+    return {
+      ...service,
+      sections: service.sections.map((section) => section.id),
+      alterations: service.alterations.map((alteration) => alteration.id),
+    };
+  }
+
+  async createService(service: Service): Promise<void> {
+    await this._db.service.create({
+      data: {
+        id: service.id,
+        englishName: service.englishName,
+        arabicName: service.arabicName,
+        hindiName: service.hindiName,
+        urduName: service.urduName,
+        bengaliName: service.bengaliName,
+        price: service.price,
+        sections: {
+          connect: service.sections.map((sectionId) => ({ id: sectionId })),
+        },
+        alterations: {
+          connect: service.alterations.map((alterationId) => ({
+            id: alterationId,
+          })),
+        },
+      },
+    });
+  }
+
+  async updateService(service: Service): Promise<void> {
+    await this._db.service.update({
+      where: { id: service.id },
+      data: {
+        englishName: service.englishName,
+        arabicName: service.arabicName,
+        hindiName: service.hindiName,
+        urduName: service.urduName,
+        bengaliName: service.bengaliName,
+        price: service.price,
+        sections: {
+          connect: service.sections.map((sectionId) => ({ id: sectionId })),
+        },
+        alterations: {
+          connect: service.alterations.map((alterationId) => ({
+            id: alterationId,
+          })),
+        },
+      },
+    });
+  }
+
+  async deleteService(id: string): Promise<void> {
+    await this._db.service.delete({
+      where: { id },
+    });
+  }
+}
