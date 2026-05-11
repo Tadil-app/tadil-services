297c5dd9452b223464fae2d544a9a0caec98c8fa
ChikhaouiFaycel <faycel@octoplus.tech>
2026-05-11 19:15:38 +0200

    fix nginx

--- apps/tadil-admin-web-client/src/integration/api-client/index.ts
+++ apps/tadil-admin-web-client/src/integration/api-client/index.ts
@@ -1,5 +1,3 @@
 import { Api } from "./tadil-api-client";
 
-export const apiClient = new Api({
-  baseURL: import.meta.env.VITE_TADIL_API_URL || "",
-}).api;
+export const apiClient = new Api().api;
